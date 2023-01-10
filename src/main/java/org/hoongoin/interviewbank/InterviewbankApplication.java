3b71063bb753ca9e1e7253f6b2d9fd341b7b3e7f
HunkiKim <gnsrl76@naver.com>
2023-01-10 13:39:10 +0900

    chore: initialize project

---
+++ src/main/java/org/hoongoin/interviewbank/InterviewbankApplication.java
@@ -0,0 +1,13 @@
+package org.hoongoin.interviewbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hoongoin/interviewbank/InterviewbankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoongoin.interviewbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
