3ff15bb003207aab8be04b1acd04876563b2b306
HunkiKim <gnsrl76@naver.com>
2023-02-24 19:42:25 +0900

    fix: delete import

--- src/main/java/org/hoongoin/interviewbank/config/SecurityConfig.java
+++ src/main/java/org/hoongoin/interviewbank/config/SecurityConfig.java
@@ -6,10 +6,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.http.HttpMethod;
 import lombok.RequiredArgsConstructor;
 
