e4cc909c272d7ae20c1e2837abfa2a7a97665a1a
yein-lee <agop0310@gmail.com>
2023-02-27 23:21:36 +0900

    refactor: redis template value type object

--- src/main/java/org/hoongoin/interviewbank/config/RedisConfig.java
+++ src/main/java/org/hoongoin/interviewbank/config/RedisConfig.java
@@ -36,8 +36,8 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, String> redisTemplate() {
-		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(connectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
