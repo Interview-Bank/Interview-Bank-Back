6a14b9393b2f49ed0a7d0baf2bc72c3fca53f413
HunkiKim <gnsrl76@naver.com>
2023-03-01 19:46:07 +0900

    fix: apply account soft delete

--- src/main/java/org/hoongoin/interviewbank/account/domain/AccountQueryService.java
+++ src/main/java/org/hoongoin/interviewbank/account/domain/AccountQueryService.java
@@ -5,6 +5,7 @@ import org.hoongoin.interviewbank.account.infrastructure.entity.AccountEntity;
 import org.hoongoin.interviewbank.account.infrastructure.repository.AccountRepository;
 import org.hoongoin.interviewbank.account.application.entity.Account;
 import org.hoongoin.interviewbank.exception.IbEntityNotFoundException;
+import org.hoongoin.interviewbank.exception.IbSoftDeleteException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -19,11 +20,24 @@ public class AccountQueryService {
 	public Account findAccountByEmail(String email) {
 		AccountEntity accountEntity = accountRepository.findByEmail(email)
 			.orElseThrow(() -> new IbEntityNotFoundException("Account"));
+
+		isDeleted(accountEntity);
+
 		return accountMapper.accountEntityToAccount(accountEntity);
 	}
 
 	public Account findAccountByAccountId(long accountId) {
-		return accountMapper.accountEntityToAccount(
-			accountRepository.findById(accountId).orElseThrow(() -> new IbEntityNotFoundException("Account")));
+		AccountEntity accountEntity = accountRepository.findById(accountId)
+			.orElseThrow(() -> new IbEntityNotFoundException("Account"));
+
+		isDeleted(accountEntity);
+
+		return accountMapper.accountEntityToAccount(accountEntity);
+	}
+
+	private void isDeleted(AccountEntity accountEntity) {
+		if (Boolean.TRUE.equals(accountEntity.getDeletedFlag())) {
+			throw new IbSoftDeleteException("Account");
+		}
 	}
 }
