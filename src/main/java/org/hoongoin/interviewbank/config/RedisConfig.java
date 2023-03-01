5c5e93669ae983f23345f8d1d1554c1fd0c9b25e
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-03-01 12:15:31 +0900

    [Fix] redis key store byte to string (#78)
    * fix: redis key store byte to string
    
    * style: removed imports not using
    
    * refactor: redis template value type object

--- src/main/java/org/hoongoin/interviewbank/config/RedisConfig.java
+++ src/main/java/org/hoongoin/interviewbank/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
@@ -31,5 +33,14 @@ public class RedisConfig {
 	@Bean
 	public SessionRegistry sessionRegistry() {
 		return new SessionRegistryImpl();
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		return redisTemplate;
 	}
 }
