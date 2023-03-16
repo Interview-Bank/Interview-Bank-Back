55503f5d9b8c026ad16fb151171f7d01999b48fd
yein-lee <agop0310@gmail.com>
2023-03-17 06:33:23 +0900

    feat: job category enum

--- src/main/java/org/hoongoin/interviewbank/interview/application/entity/DevelopSecondaryJobCategory.java
+++ src/main/java/org/hoongoin/interviewbank/interview/application/entity/DevelopSecondaryJobCategory.java
@@ -1,2 +1,21 @@
-package org.hoongoin.interviewbank.interview.application.entity;public class DevelopSecondaryJobCategory {
+package org.hoongoin.interviewbank.interview.application.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DevelopSecondaryJobCategory {
+
+	BACKEND("백엔드"),
+	FRONTEND("프런트엔드"),
+	ANDROID("안드로이드"),
+	IOS("IOS"),
+	MOBILE("모바일"),
+	DEVOPS("DevOps"),
+	QA("QA"),
+	GAME("게임"),
+	AI("AI");
+
+	private final String developSecondaryJobName;
 }
--- src/main/java/org/hoongoin/interviewbank/interview/application/entity/PrimaryJobCategory.java
+++ src/main/java/org/hoongoin/interviewbank/interview/application/entity/PrimaryJobCategory.java
@@ -1,2 +1,18 @@
-package org.hoongoin.interviewbank.interview.application.entity;public enum PrimaryJobCategory {
+package org.hoongoin.interviewbank.interview.application.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PrimaryJobCategory {
+
+	DEVELOP("개발"),
+	R_N_D("R&D"),
+	DESIGN("디자인"),
+	PLANNING("기획/PM"),
+	MARKETING("마케팅"),
+	ETC("기타");
+
+	private final String primaryJobName;
 }
