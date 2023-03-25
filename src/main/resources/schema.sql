2fabc913f4e24ba569e55017efc1484dd6318b0c
yein-lee <agop0310@gmail.com>
2023-03-25 17:53:47 +0900

    refactor: separate job category dml

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -86,19 +86,3 @@ CREATE TABLE `scrap_answer`
     PRIMARY KEY (`id`),
     CONSTRAINT FOREIGN KEY (`scrap_question_id`) REFERENCES `scrap_question` (`id`)
 );
-
-INSERT INTO job_category (id, name, parent_id) VALUES (1, '개발', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (2, 'R&D', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (3, '디자인', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (4, '기획/PM', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (5, '마케팅', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (6, '기타', null);
-INSERT INTO job_category (id, name, parent_id) VALUES (7, '백엔드', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (8, '프런트엔드', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (9, '안드로이드', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (10, 'IOS', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (11, '모바일', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (12, 'DevOps', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (13, 'QA', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (14, '게임', 1);
-INSERT INTO job_category (id, name, parent_id) VALUES (15, 'AI', 1);
