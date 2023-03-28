c4591a3532275c2f19d4a73ff7c6a90c1db42513
HunkiKim <gnsrl76@naver.com>
2023-03-29 05:49:13 +0900

    fix: fix jenkins shell for swagger

--- jenkins-trigger.sh
+++ jenkins-trigger.sh
@@ -1,7 +1,8 @@
-cd /home/ubuntu/Interview-Bank-Back/interview_bank_docker
+cd /home/ubuntu/Interview-Bank-Back
 git pull origin staging
+./gradlew build
+cd /home/ubuntu/Interview-Bank-Back/interview_bank_docker
 docker-compose -f docker-compose-staging.yaml down
 docker system prune --volumes --force
 docker-compose -f docker-compose-staging.yaml build --no-cache
 docker-compose -f docker-compose-staging.yaml up -d
-
