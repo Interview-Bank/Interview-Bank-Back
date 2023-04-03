ca32cdc30a8846e38c0d46a9c8bcfc993ce115b9
yein-lee <agop0310@gmail.com>
2023-04-03 12:21:39 +0900

    fix: schema data type

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -1,13 +1,13 @@
 CREATE TABLE account
 (
-    id           BIGINT                NOT NULL AUTO_INCREMENT,
-    created_at   TIMESTAMP(6),
-    deleted_at   TIMESTAMP(6),
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6),
+    deleted_at TIMESTAMP(6),
     deleted_flag BOOLEAN DEFAULT FALSE NOT NULL,
-    updated_at   TIMESTAMP(6),
-    email        VARCHAR(120)          NOT NULL,
-    nickname     VARCHAR(20)           NOT NULL,
-    password     VARCHAR(61),
+    updated_at TIMESTAMP(6),
+    email CHAR(120) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    password CHAR(61),
     account_type CHAR(6),
     PRIMARY KEY (id)
 );
@@ -18,20 +18,22 @@ CREATE TABLE `job_category` (
     parent_id BIGINT,
     created_at DATETIME,
     updated_at DATETIME,
-    PRIMARY KEY (id),
-    CONSTRAINT FOREIGN KEY (parent_id) REFERENCES job_category (id)
+    PRIMARY KEY (id)
 );
 
+ALTER TABLE `job_category`
+    ADD CONSTRAINT `fk_job_category_parent_id`
+    FOREIGN KEY (parent_id) REFERENCES `job_category` (id);
 
 CREATE TABLE interview
 (
-    id           BIGINT                NOT NULL AUTO_INCREMENT,
-    created_at   TIMESTAMP(6),
-    deleted_at   TIMESTAMP(6),
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6),
+    deleted_at TIMESTAMP(6),
     deleted_flag BOOLEAN DEFAULT FALSE NOT NULL,
-    updated_at   TIMESTAMP(6),
-    title        VARCHAR(128)          NOT NULL,
-    account_id   BIGINT                NOT NULL,
+    updated_at TIMESTAMP(6),
+    title CHAR(128) NOT NULL,
+    account_id BIGINT NOT NULL,
     job_category_id BIGINT,
     PRIMARY KEY (id),
     CONSTRAINT FK_account_id FOREIGN KEY (account_id) REFERENCES account (id),
@@ -40,25 +42,25 @@ CREATE TABLE interview
 
 CREATE TABLE question
 (
-    id           BIGINT                NOT NULL AUTO_INCREMENT,
-    created_at   TIMESTAMP(6),
-    deleted_at   TIMESTAMP(6),
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6),
+    deleted_at TIMESTAMP(6),
     deleted_flag BOOLEAN DEFAULT FALSE NOT NULL,
-    updated_at   TIMESTAMP(6),
-    content      LONGTEXT              NOT NULL,
-    interview_id BIGINT                NOT NULL,
+    updated_at TIMESTAMP(6),
+    content VARCHAR NOT NULL,
+    interview_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_interview_id FOREIGN KEY (interview_id) REFERENCES interview (id)
 );
 
 CREATE TABLE scrap
 (
-    id           BIGINT      NOT NULL AUTO_INCREMENT,
-    created_at   TIMESTAMP(6),
-    updated_at   TIMESTAMP(6),
-    title        VARCHAR(50) NOT NULL,
-    account_id   BIGINT      NOT NULL,
-    interview_id BIGINT      NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    title CHAR(128) NOT NULL,
+    account_id BIGINT NOT NULL,
+    interview_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_account_id_2 FOREIGN KEY (account_id) REFERENCES account (id),
     CONSTRAINT FK_interview_id_2 FOREIGN KEY (interview_id) REFERENCES interview (id)
@@ -66,21 +68,21 @@ CREATE TABLE scrap
 
 CREATE TABLE scrap_question
 (
-    id         BIGINT        NOT NULL AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     created_at TIMESTAMP(6),
     updated_at TIMESTAMP(6),
-    content    VARCHAR(1000) NOT NULL,
-    scrap_id   BIGINT        NOT NULL,
+    content VARCHAR NOT NULL,
+    scrap_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_scrap_id FOREIGN KEY (scrap_id) REFERENCES scrap (id)
 );
 
 CREATE TABLE scrap_answer
 (
-    id                BIGINT NOT NULL AUTO_INCREMENT,
-    created_at        TIMESTAMP(6),
-    updated_at        TIMESTAMP(6),
-    content           VARCHAR(5000),
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    created_at TIMESTAMP(6),
+    updated_at TIMESTAMP(6),
+    content VARCHAR,
     scrap_question_id BIGINT NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_scrap_question_id FOREIGN KEY (scrap_question_id) REFERENCES scrap_question (id)
@@ -88,10 +90,10 @@ CREATE TABLE scrap_answer
 
 CREATE TABLE `inquiry` (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(128) NOT NULL,
-    email VARCHAR(120) NOT NULL,
-    content VARCHAR(100000) NOT NULL,
-    attachedFileUrl VARCHAR(255),
+    title CHAR(128) NOT NULL,
+    email CHAR(120) NOT NULL,
+    content VARCHAR NOT NULL,
+    attachedFileUrl CHAR(255),
     isAnswered BOOLEAN NOT NULL,
     created_at DATETIME(6) NOT NULL,
     updated_at DATETIME(6) NOT NULL,
