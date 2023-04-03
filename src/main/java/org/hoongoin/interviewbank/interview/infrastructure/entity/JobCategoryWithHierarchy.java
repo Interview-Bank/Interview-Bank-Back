514fb550359838d42611911507fd65adeaaae6d6
yein-lee <agop0310@gmail.com>
2023-04-03 21:08:17 +0900

    chore: db job category

---
+++ src/main/java/org/hoongoin/interviewbank/interview/infrastructure/entity/JobCategoryWithHierarchy.java
@@ -0,0 +1,18 @@
+package org.hoongoin.interviewbank.interview.infrastructure.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobCategoryDTO {
+
+	private Long firstLevelId;
+	private String firstLevelName;
+	private Long secondLevelId;
+	private String secondLevelName;
+}
