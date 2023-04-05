95486edff8d4a85beac36b1b14ddd807dd152889
yein-lee <agop0310@gmail.com>
2023-04-05 11:55:25 +0900

    fix: job category multi select

--- src/main/java/org/hoongoin/interviewbank/interview/infrastructure/repository/InterviewRepository.java
+++ src/main/java/org/hoongoin/interviewbank/interview/infrastructure/repository/InterviewRepository.java
@@ -3,6 +3,7 @@ package org.hoongoin.interviewbank.interview.infrastructure.repository;
 import static org.hoongoin.interviewbank.interview.infrastructure.entity.QInterviewEntity.*;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hoongoin.interviewbank.interview.infrastructure.entity.InterviewEntity;
 import org.springframework.data.domain.Page;
@@ -23,13 +24,13 @@ public interface InterviewRepository extends JpaRepository<InterviewEntity, Long
 
 	@EntityGraph(attributePaths = {"accountEntity"})
 	@Query("SELECT interview FROM InterviewEntity interview "
-		+ "WHERE (:job_category_id IS NULL OR interview.jobCategoryEntity.id = :job_category_id ) "
+		+ "WHERE (:job_category_ids IS NULL OR interview.jobCategoryEntity.id IN :job_category_ids ) "
 		+ "AND (:query IS NULL OR interview.title LIKE %:query%) "
 		+ "AND (:start_date IS NULL OR interview.createdAt >= TIMESTAMP(:start_date)) "
 		+ "AND (:end_date IS NULL OR interview.createdAt <= TIMESTAMP(:end_date)) "
 		+ "ORDER BY interview.createdAt DESC")
-	Page<InterviewEntity> findAllByTitleAndJobCategoryIdAndStartDateAndEndDatePageableOrderByCreateTimeAsc(
-		@Param("query") String query, @Param("job_category_id") Long jobCategoryId, @Param("start_date") Date startDate,
+	Page<InterviewEntity> findAllByTitleAndJobCategoryIdsAndStartDateAndEndDatePageableOrderByCreateTimeAsc(
+		@Param("query") String query, @Param("job_category_ids") List<Long> jobCategoryIds, @Param("start_date") Date startDate,
 		@Param("end_date") Date endDate, Pageable pageable);
 
 	default Page<InterviewEntity> findByAccountEntityIdAndDeleteFlag(Pageable pageable, long accountId) {
