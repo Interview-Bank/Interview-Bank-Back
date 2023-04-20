1bf7d76fa24753edb605bbba1dd4dc0521fce308
yein-lee <agop0310@gmail.com>
2023-04-20 18:54:29 +0900

    fix: kakao oauth get user info null check

--- src/main/java/org/hoongoin/interviewbank/account/application/KakaoOAuthService.java
+++ src/main/java/org/hoongoin/interviewbank/account/application/KakaoOAuthService.java
@@ -70,12 +70,16 @@ public class KakaoOAuthService {
 
 		HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
 
-		return restTemplate.exchange(
+		KakaoUserInfoResponse kakaoUserInfoResponse = restTemplate.exchange(
 				kakaoOAuthProperties.getProfileUri(),
 				HttpMethod.POST,
 				kakaoProfileRequest,
 				KakaoUserInfoResponse.class)
 			.getBody();
+		if (kakaoUserInfoResponse == null) {
+			throw new IbInternalServerException("Failed to get user info from Kakao");
+		}
+		return kakaoUserInfoResponse;
 	}
 
 	private Account getAccount(KakaoUserInfoResponse kakaoUserInfoResponse) {
