7339c742e8ebc3d53db2e4cf69f14a8de8a08947
HunkiKim <gnsrl76@naver.com>
2023-04-21 17:53:30 +0900

    fix: delete db setting

--- jenkins-trigger-production.sh
+++ jenkins-trigger-production.sh
@@ -1,4 +1,4 @@
-cd /home/ubuntu/Interview-Bank-Back/interview_bank_docker
+cd ./interview_bank_docker
 docker-compose -f docker-compose-production.yaml down
 docker system prune --volumes --force
 docker-compose -f docker-compose-staging.yaml build --no-cache
