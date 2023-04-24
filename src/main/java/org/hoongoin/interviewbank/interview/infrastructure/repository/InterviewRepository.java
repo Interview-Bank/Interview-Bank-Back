685ad334df440f54f54b93040cbdbb77b4805a51
Yein Lee (이예인) <39490214+yein-lee@users.noreply.github.com>
2023-04-24 10:06:35 +0900

    [Fix] interview search (#202)
    * fix: interview search add career year
    
    * fix: career year and interview period
    
    * fix: search query
    
    * fix: search query

--- src/main/java/org/hoongoin/interviewbank/interview/infrastructure/repository/InterviewRepository.java
+++ src/main/java/org/hoongoin/interviewbank/interview/infrastructure/repository/InterviewRepository.java
@@ -5,6 +5,7 @@ import static org.hoongoin.interviewbank.interview.infrastructure.entity.QInterv
 import java.util.Date;
 import java.util.List;
 
+import org.hoongoin.interviewbank.interview.enums.CareerYear;
 import org.hoongoin.interviewbank.interview.enums.InterviewPeriod;
 import org.hoongoin.interviewbank.interview.infrastructure.entity.InterviewEntity;
 import org.springframework.data.domain.Page;
@@ -30,10 +31,12 @@ public interface InterviewRepository extends JpaRepository<InterviewEntity, Long
 		+ "AND (:start_date IS NULL OR interview.createdAt >= TIMESTAMP(:start_date)) "
 		+ "AND (:end_date IS NULL OR interview.createdAt <= TIMESTAMP(:end_date)) "
 		+ "AND (:interview_period IS NULL OR interview.interviewPeriod = :interview_period) "
+		+ "AND (:career_year IS NULL OR interview.careerYear = :career_year) "
 		+ "ORDER BY interview.createdAt DESC")
 	Page<InterviewEntity> findAllByTitleAndJobCategoryIdsAndStartDateAndEndDatePageableOrderByCreateTimeDesc(
 		@Param("query") String query, @Param("job_category_ids") List<Long> jobCategoryIds, @Param("start_date") Date startDate,
-		@Param("end_date") Date endDate, @Param("interview_period") InterviewPeriod interviewPeriod, Pageable pageable);
+		@Param("end_date") Date endDate, @Param("interview_period") InterviewPeriod interviewPeriod,
+		@Param("career_year") CareerYear careerYear, Pageable pageable);
 
 	default Page<InterviewEntity> findByAccountEntityIdAndDeleteFlag(Pageable pageable, long accountId) {
 		BooleanExpression hasAccountId = interviewEntity.accountEntity.id.eq(accountId);
