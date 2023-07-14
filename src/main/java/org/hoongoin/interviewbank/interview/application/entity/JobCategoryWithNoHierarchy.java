355db5b7fcdc0778ad9508f86eca98cd811b4b26
yein-lee <agop0310@gmail.com>
2023-07-14 14:50:55 +0900

    feat: get job categories with no hieararchy

---
+++ src/main/java/org/hoongoin/interviewbank/interview/application/entity/JobCategoryWithNoHierarchy.java
@@ -0,0 +1,12 @@
+package org.hoongoin.interviewbank.interview.application.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class JobCategoryWithNoHierarchy {
+
+	private long jobCategoryId;
+	private String name;
+}
