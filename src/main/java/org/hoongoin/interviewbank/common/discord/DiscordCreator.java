60e20814999c102719d99a07aa79b5c5424be4e5
yein-lee <agop0310@gmail.com>
2023-07-26 13:18:29 +0900

    fix: message contents

--- src/main/java/org/hoongoin/interviewbank/common/discord/DiscordCreator.java
+++ src/main/java/org/hoongoin/interviewbank/common/discord/DiscordCreator.java
@@ -5,19 +5,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
 
 @Component
 public class DiscordCreator {
 
 	private final DiscordClient discordClient;
+	private final GatewayDiscordClient gatewayDiscordClient;
 
 	@Autowired
 	public DiscordCreator(@Value("${discord.token}") String discordToken) {
 		this.discordClient = DiscordClient.create(discordToken);
-		discordClient.login().block();
+		this.gatewayDiscordClient = discordClient.login().block();
 	}
 
 	public DiscordClient getDiscordClient() {
 		return discordClient;
+	}
+
+	public GatewayDiscordClient getGatewayDiscordClient() {
+		return gatewayDiscordClient;
 	}
 }
